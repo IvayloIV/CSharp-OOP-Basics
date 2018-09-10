ba97b8ca9dba04b145f63d23a61db1023adb99de
IvayloIV <ivvo98@abv.bg>
2018-09-10 16:33:21 +0300

    Lab-Iterators_and_Comparators

---
+++ C#-OOP-Basics-February-2018/Lab-Iterators_and_Comparators/02.LibraryIterator/Library.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+
+public class Library : IEnumerable<Book>
+{
+    public Library(params Book[] books)
+    {
+        this.Books = new List<Book>(books);
+    }
+
+    public List<Book> Books { get; private set; }
+
+    public IEnumerator<Book> GetEnumerator()
+    {
+        return new LibraryIterator(this.Books);
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+
+    private class LibraryIterator : IEnumerator<Book>
+    {
+        public LibraryIterator(List<Book> books)
+        {
+            this.Books = new List<Book>(books);
+            this.CurrentIndex = -1;
+        }
+
+        public List<Book> Books { get; private set; }
+        public int CurrentIndex { get; private set; }
+
+        public Book Current => this.Books[this.CurrentIndex];
+
+        object IEnumerator.Current => this.Current;
+
+        public void Dispose() { }
+
+        public bool MoveNext()
+        {
+            this.CurrentIndex++;
+            return this.CurrentIndex < this.Books.Count;
+        }
+
+        public void Reset()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ C#-OOP-Basics-February-2018/Lab-Iterators_and_Comparators/04.BookComparer/BookComparator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+public class BookComparator : IComparer<Book>
+{
+    public int Compare(Book x, Book y)
+    {
+        var diff = x.Title.CompareTo(y.Title);
+        if (diff == 0) 
+        {
+            diff = y.Year.CompareTo(x.Year);
+        }
+        return diff;
+    }
+}
