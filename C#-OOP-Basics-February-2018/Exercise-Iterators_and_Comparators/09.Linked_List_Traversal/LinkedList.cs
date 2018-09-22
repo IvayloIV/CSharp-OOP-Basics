f3d8ba77d90ee2a17266e9a6fff7113ab74d09e3
IvayloIV <ivvo98@abv.bg>
2018-09-22 12:48:49 +0300

    add new task

---
+++ C#-OOP-Basics-February-2018/Exercise-Iterators_and_Comparators/09.Linked_List_Traversal/LinkedList.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class LinkedList<T> : IEnumerable<T>
+{
+    public int Count { get; private set; }
+
+    private Node Head { get; set; }
+
+    private Node Tail { get; set; }
+
+
+    public void Add(T element)
+    {
+        var newNode = new Node(element);
+        if (this.Count == 0)
+        {
+            this.Tail = this.Head = newNode;
+        }
+        else
+        {
+            newNode.Prev = this.Tail;
+            this.Tail.Next = newNode;
+            this.Tail = newNode;
+        }
+        this.Count++;
+    }
+
+    public bool Remove(T element)
+    {
+        var tempHead = this.Head;
+        while (tempHead != null)
+        {
+            if (EqualityComparer<T>.Default.Equals(tempHead.Value, element))
+            {
+                var lastElement = tempHead.Prev;
+                var nextElement = tempHead.Next;
+
+                if (lastElement != null)
+                {
+                    lastElement.Next = nextElement;
+                }
+                else
+                {
+                    this.Head = this.Head.Next;
+                }
+                if (nextElement != null)
+                {
+                    nextElement.Prev = lastElement;
+                }
+                else
+                {
+                    this.Tail = this.Tail.Prev;
+                }
+                this.Count--;
+                return true;
+            }
+            tempHead = tempHead.Next;
+        }
+        return false;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        var head = this.Head;
+        while (head != null)
+        {
+            yield return head.Value;
+            head = head.Next;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+
+    private class Node
+    {
+        public T Value { get; private set; }
+
+        public Node Prev { get; set; }
+
+        public Node Next { get; set; }
+
+        public Node(T value)
+        {
+            this.Value = value;
+        }
+    }
+}
---
+++ C#-OOP-Basics-February-2018/Exercise-Iterators_and_Comparators/09.Linked_List_Traversal/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _09.Linked_List_Traversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var linkedList = new LinkedList<int>();
+            var type = typeof(LinkedList<int>);
+            var n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                var tokens = Console.ReadLine().Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+                var methodName = tokens[0];
+                var value = int.Parse(tokens[1]);
+                var method = type.GetMethod(methodName);
+                method.Invoke(linkedList, new object[] { value });
+            }
+
+            PrintResult(linkedList);
+        }
+
+        private static void PrintResult(LinkedList<int> linkedList)
+        {
+            Console.WriteLine(linkedList.Count);
+            foreach (var item in linkedList)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
